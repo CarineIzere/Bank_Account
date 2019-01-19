a9b960661870d9a677e56b4bd7bb3ff59ce58d7a
CarineIzere <carizeree@gmail.com>
2019-01-19 19:00:38 +0200

    add html

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+function Account(name, balance) {
+  this.name = name;
+  this.balance = balance;
+}
+
+Account.prototype.deposit = function(amount) {
+  if (this._isPositive(amount)) {
+    this.balance += amount;
+    console.info(`Deposit: ${this.name} new balance is ${this.balance}`);
+    return true;
+  }
+  return false;
+}
+
+Account.prototype.withdraw = function(amount) {
+  if (this._isAllowed(amount)) {
+    this.balance -= amount;
+    console.info(`Withdraw: ${this.name} new balance is ${this.balance}`);
+    return true;
+  }
+  return false;
+}
